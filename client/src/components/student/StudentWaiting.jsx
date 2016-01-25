3dc3739cd98ba5b77a76d1f43e4fa265c5369c5b
Lex <lex.cooke@gmail.com>
2016-01-25 15:18:37 -0800

    add student waiting view

---
+++ client/src/components/student/StudentWaiting.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    return <div className="waiting">
+      You are connected. Please wait for teacher prompt.
+    </div>;
+  }
+});
