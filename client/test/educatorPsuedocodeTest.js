bfb91325a20f2f1329deb405919c7b49e5925430
henry <hlyford11@gmail.com>
2016-01-25 14:35:27 -0800

    psuedo code for client side tests - teacher and student

---
+++ client/test/educatorPsuedocodeTest.js
@@ -0,0 +1,6 @@
+// an educator should be able to create a new room from the home page
+
+// educator should be able to request a checkin after the room is created
+	// checkin page should show the tally of votes and a 'end check-in' button
+
+// ending a checkin should return the edcucator to the page where they can request a check-in
