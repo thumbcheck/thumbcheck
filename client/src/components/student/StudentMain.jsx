aa391439ec1370eb1345c6ab8c3f7ca661bf52b2
Lex <lex.cooke@gmail.com>
2016-01-25 20:50:32 -0800

    connect student main and answering with state

--- client/src/components/student/StudentMain.jsx
+++ client/src/components/student/StudentMain.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Answer from './StudentAnswering';
 import Wait from './StudentWaiting';
-import * as actionCreators from '../action_creators';
+import Answer from './StudentAnswering';
+import * as actionCreators from '../../action_creators';
 
 export const Student = React.createClass({
   render: function() {
-    return <div>
-      {this.props.wait ?
-        <Wait {...this.props} /> :
-        <Answer ref="answer" upvote={this.props.upvote} downvote={this.props.downvote} />}
-    </div>;
+    return (
+      <div>
+        {this.props.voting ?
+          <Answer ref="answer" upvote={this.props.upvote} downvote={this.props.downvote} /> :
+          <Wait />}
+      </div>
+    );
   }
 });
 
 function mapStateToProps(state) {
   return {
-    wait: state.get('wait'),
-    upvote: state.get('upvote'),
-    downvote: state.get('downvote')
+    voting: state.get('voting'),
+    upvote: state.getIn(['tally', 'thumbsUp']),
+    downvote: state.getIn(['tally', 'thumbsDown'])
   };
 }
 
