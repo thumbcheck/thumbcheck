1573d32d9f8c5bbcc10e49fa20be92266b2caa37
m-sandusky <msandusky08@gmail.com>
2016-01-26 16:20:29 -0800

    Implements endvote function for educator

--- client/src/components/educator/ResultsDisplay.jsx
+++ client/src/components/educator/ResultsDisplay.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 export default React.createClass({
   render: function() {
     console.log("resultsdisplay props", this.props);
-    return <div className="results-container">
+    return (
+      <div className="results-container">
 
-      <div className="up-thumb-count">{this.props.upCount}</div>
-      <div className="down-thumb-count">{this.props.downCount}</div>
-      
-      <button className="end-thumb-check"
-              onClick={() => this.props.endVote()}>
-      </button>
-    </div>;
+        <div className="up-thumb-count">{this.props.upCount}</div>
+        <div className="down-thumb-count">{this.props.downCount}</div>
+        
+        <button className="end-thumb-check" onClick={this.props.stopVote}>
+        END VOTE
+        </button>
+      </div>
+    );
   }
 });
