54734f667ed04011741e5a240258f077b5ad1fe4
henry <hlyford11@gmail.com>
2016-01-28 15:57:45 -0800

    Add basic charts for thumbs check using react-chart.js; style opinions welcome

--- client/src/components/educator/ResultsDisplay.jsx
+++ client/src/components/educator/ResultsDisplay.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import BarGraph from './EducatorBarGraph';
 
-export default React.createClass({
+export default React.createClass({  
+
   render: function() {
+    
     return (
-      <div className="results-container">
-
-        <div className="up-thumb-count">Thumbs up: {this.props.upCount}</div>
-        <div className="down-thumb-count">Thumbs down: {this.props.downCount}</div>
-        
+      <div className="results-container">        
+        <BarGraph ref="resultsDisplay" {...this.props} />
+        <div>
+          <span className="up-thumb-count">Thumbs up count: {this.props.upCount}</span>
+          <span className="down-thumb-count move-right">Thumbs down count: {this.props.downCount}</span>
+        </div>
         <button className="btn orange request-btn white-text end-thumb-check" onClick={this.props.stopVote}>
         END VOTE
         </button>
       </div>
-    );
+    );    
+    
   }
 });
