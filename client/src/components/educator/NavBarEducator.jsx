1d3bf4f490d7c83badd691fb315ec2fe3c673859
henry <hlyford11@gmail.com>
2016-01-27 18:43:54 -0800

    Basic styling for both types of account

--- client/src/components/educator/NavBarEducator.jsx
+++ client/src/components/educator/NavBarEducator.jsx
@@ -19,13 +19,13 @@ export default React.createClass({
 			      
 			      <ul className="nav navbar-nav navbar-right">			        
 			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Options<span className="caret"></span></a>
 			          <ul className="dropdown-menu">
 			            <li><a href="#">Action</a></li>
 			            <li><a href="#">Another action</a></li>
 			            <li><a href="#">Something else here</a></li>
 			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
+			            <li><a href="#"></a></li>
 			          </ul>
 			        </li>
 			      </ul>
