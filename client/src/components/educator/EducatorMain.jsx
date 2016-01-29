e79c70f6dc59757613c7b65c6a64699ba1984977
m-sandusky <msandusky08@gmail.com>
2016-01-29 12:19:21 -0800

    Removes need for local storage on client side

--- client/src/components/educator/EducatorMain.jsx
+++ client/src/components/educator/EducatorMain.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {connect} from 'react-redux';
 import EducatorRequestCheckin from './EducatorRequestCheckin';
@@ -7,33 +8,36 @@ import Navbar from './NavBarEducator';
 import * as actionCreators from '../../action_creators';
 
 export const Educator = React.createClass({
-  componentDidMount: function() {
-    this.props.stopVote();
-  },
-  render: function() {    
-    return (
-      <div>
-        <Navbar />          
-        <div className="educator-container">
-          <div className="center-text">
-            <h4>Share this URL with your students: {window.location.href.split('?')[0]}</h4>
-            {this.props.voting ?
-              <ResultsDisplay ref="resultsDisplay" {...this.props} /> :
-              <EducatorRequestCheckin {...this.props} />
-            }
-          </div>
-        </div>
-      </div>
-    )
-  }
-});    	
+ componentDidMount: function() {
+   this.props.stopVote();
+ },
+ render: function() {    
+   console.log('state educator', this.props);
+   return (
+     <div>
+       <Navbar />          
+       <div className="educator-container">
+         <div className="center-text">
+           <h4>Share this URL with your students: {window.location.href.split('?')[0]}</h4>
+           {this.props.voting ?
+             <ResultsDisplay ref="resultsDisplay" {...this.props} /> :
+             <EducatorRequestCheckin {...this.props} />
+           }
+         </div>
+       </div>
+     </div>
+   )
+ }
+});        
 
-function mapStateToProps(state) {
-  return {
-    voting: state.get('voting'),
-    upCount: state.getIn(['tally', 'thumbsUp']),
-    downCount: state.getIn(['tally', 'thumbsDown'])
-  };
-}
+// function mapStateToProps(state) {
+//  return {
+//    voting: state.get('voting'),
+//    upCount: state.getIn(['tally', 'thumbsUp']),
+//    downCount: state.getIn(['tally', 'thumbsDown'])
+//  };
+// }
 
-export const EducatorContainer = connect(mapStateToProps,actionCreators)(Educator);
+// const EducatorContainer = Educator;
+// export const EducatorContainer;
+// export const EducatorContainer = connect(mapStateToProps,actionCreators)(Educator);
