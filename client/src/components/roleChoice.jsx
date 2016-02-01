abf6564fb2b03f68d1373d4e0f8d409948eac61f
Kris Albert Lee <krisalbert@gmail.com>
2016-02-01 15:22:56 -0800

    Teacher can provide roomname

--- client/src/components/roleChoice.jsx
+++ client/src/components/roleChoice.jsx
@@ -7,7 +7,7 @@ export default React.createClass({
   },
 
   chooseStudent: function () {
-    this.props.chooseRole('student');
+    this.props.chooseRole('');
   },
 
   onChange: function (e) {
@@ -16,26 +16,23 @@ export default React.createClass({
 
   render: function() {
     //console.log(this.props.choice);
-    if(!this.props.choice){
-      return (
-        <p><a className="btn btn-primary btn-md" role="button" onClick={this.chooseTeacher} >Teacher</a>
-        <a className="btn btn-primary btn-md" role="button" onClick={this.chooseStudent} >Student</a></p>
-      )
-    } else if(this.props.choice === 'teacher'){
-      return (
-        <div>
-          <p>Enter roomname here <input onChange={this.onChange} /> <a className="btn btn-primary btn-md" role="button" href={"/"+this.props.roomName+"?type=host"} >Create</a></p>
-          <p><a className="btn btn-success btn-lg" href="/room" role="button">Generate Random Roomname</a></p>
-          <p>Student? Click here to join a room instead. <a className="btn btn-primary btn-md" role="button" onClick={this.chooseStudent} >Student</a></p>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <p>Enter roomname here <input onChange={this.onChange} /> <a className="btn btn-primary btn-md" role="button" href={"/"+this.props.roomName} >Join</a></p>
-          <p>Teacher? Click here to create a room instead. <a className="btn btn-primary btn-md" role="button" onClick={this.chooseTeacher} >Teacher</a></p>
-        </div>
-      )
-    }
+    //let teacher = false;
+    //console.log(teacher);
+    return (
+      <div>
+        {!this.props.choice ?
+          <div>
+            <p>Enter roomname <input onChange={this.onChange} /> <a className="btn btn-primary btn-md" role="button" href={"/"+this.props.roomName} >Join</a></p>
+            <a className="btn btn-warning btn-md" role="button" onClick={this.chooseTeacher} >Teacher</a>
+          </div>
+          :
+            <div>
+              <p>Create custom room <input onChange={this.onChange} /> <a className="btn btn-primary btn-md" role="button" href={"/"+this.props.roomName+"?type=host"} >Create</a></p>
+              <p><a className="btn btn-success btn-lg" href="/room" role="button">Generate Random Roomname</a></p>
+              <a className="btn btn-warning btn-md" role="button" onClick={this.chooseStudent} >Student</a>
+            </div>
+        }
+      </div>
+    )
   }
 });
