c47050c5dc9a0e9a0f34a2ff9f2bc864740e4262
m-sandusky <msandusky08@gmail.com>
2016-02-01 15:28:28 -0800

    Adds uuid generator function file

---
+++ client/src/generateID.js
@@ -0,0 +1,10 @@
+import uuid from 'uuid';
+
+export default function generateID() {
+  let id = localStorage.getItem('participantID');
+  if (!id) {
+    id = uuid.v4();
+    localStorage.setItem('participantID', id);
+  }
+  return id;
+}
