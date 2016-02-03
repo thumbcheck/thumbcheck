3f2fe37f50a2369fbfdc296322a8fadfa370dc05
m-sandusky <msandusky08@gmail.com>
2016-02-03 12:11:39 -0800

    Refactors socket server side code

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ import socket from 'socket.io';
 import bodyParser from 'body-parser';
 import path from 'path';
 import router from './router';
-import {toJS} from 'immutable';
-import sendNumUsers from './helpers/sendNumUsers';
-import {storeState, retrieveState} from './controllers/redisStateController';
+import sockets from './sockets';
 
 const port = process.env.PORT || 8090;
 
@@ -19,41 +17,7 @@ const server = app.listen(port, ()  => {
   console.log('Server listening at port ', port);
 });
 
+
 var io = socket.listen(server);
+sockets(io);
 
-io.on('connection', (socket) => {
-
-  socket.on('action', (action) => {
-    action.meta.remote = false;
-    socket.broadcast.to(socket.room).emit('remoteAction', action);
-  });
-
-  socket.on('state', (state) => {
-    storeState(state);
-  });
-
-  socket.on('joinRoom', (roomname) => {
-    socket.join(roomname);
-    
-    socket.room = roomname;
-    retrieveState(roomname, (err, appState) => {
-      if (err) throw new Error(err);
-      socket.emit('syncState', appState);
-    });
-
-    let numUsers = io.sockets.adapter.rooms[roomname].length;
-    sendNumUsers(numUsers, socket, io);
-  });
-
-  socket.on('disconnect', () => {
-    if (io.sockets.adapter.rooms[socket.room]) {
-      let numUsers = io.sockets.adapter.rooms[socket.room].length;
-      sendNumUsers(numUsers, socket, io);
-    }
-  });
-
-  socket.on('error', (err) => {
-    if(err) throw new Error(err);
-  });
-
-});
