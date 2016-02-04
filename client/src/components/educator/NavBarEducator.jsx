d7715ed5c6558c79e0f448ffbd445369de53d6fd
henry <hlyford11@gmail.com>
2016-02-04 14:19:23 -0800

    bubble notifications for questions

--- client/src/components/educator/NavBarEducator.jsx
+++ client/src/components/educator/NavBarEducator.jsx
@@ -15,12 +15,24 @@ export default React.createClass({
 	  	let questions = this.props.questions.toJS();
 	  	return questions.map(function(tuple, index) {
 	    	return <li className="questions-list">{tuple[1]}<span className="remove-icon remove-right" onClick={removeQuestion.bind((tuple[0]))} aria-hidden="true"><img className="remove-icon" src="/images/icons/remove.png"/></span></li>
-	  	});
-	  }
+	  	});   
+	  } else {
+	  	return <li className="questions-list">No questions currently</li>
+	  } 
   },
-
-  render: function() {
-    return (
+  showBubble: function(length) {
+  	if (length) {
+  		if (this.props.questions) {
+  			return this.props.questions.size;
+  		}
+  	} else {
+  		if (this.props.questions && this.props.questions.size > 0) return "noti_bubble";
+  		else return "dont-show";
+  	}
+  	return null;  		
+  },
+  render: function() {  	
+    return ( 
     	<nav className="navbar navbar-default">
 			  <div className="container-fluid blue">
 			    <div className="navbar-header">
@@ -38,7 +50,11 @@ export default React.createClass({
 
 			      <ul className="nav navbar-nav navbar-right">
 			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle white-text right-nav" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img data-placement="bottom" title="See raised hands" src="/images/raise_hand1.png" className="raise-hand-icon" /><span className="caret"></span></a>
+			          <a href="#" className="dropdown-toggle white-text right-nav" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			          	<img data-placement="bottom" title="See raised hands" src="/images/raise_hand1.png" className="raise-hand-icon" />
+			          	<div className={this.showBubble()} ><span className="">{ this.showBubble('length')} </span> </div>
+			          	<span className="caret"></span>
+			          </a>
 			          <ul className="dropdown-menu drop-down-spacing">
 			            { this.showQuestions() }
 			          </ul>
