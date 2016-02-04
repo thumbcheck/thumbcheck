4654a6d3d2e32a18d619e8d0bd75d0ccb2a7cb0a
m-sandusky <msandusky08@gmail.com>
2016-02-04 12:22:06 -0800

    Finishes refactor of reducer and Main Components

--- client/src/components/MainLanding.jsx
+++ client/src/components/MainLanding.jsx
@@ -6,7 +6,7 @@ import Navbar from './educator/NavBarEducator';
 import * as actionCreators from '../action_creators';
 import PrivateBrowsingPage from './PrivateBrowsingPage';
 import RoleChoice from './RoleChoice';
-
+import mapStateToProps from '../helpers/mapStateToProps';
 
 export const Main = React.createClass({
   canWriteLocalStorage: function() {
@@ -42,34 +42,34 @@ export const Main = React.createClass({
   }
 });
 
-function mapStateToProps(state) {
- return {
-    userType: state.get('userType'),
-    currentRoom : state.get('currentRoom'),
-    voting: state.get('voting'),
-    upCount: state.getIn(['tally', 'thumbsUp']),
-    downCount: state.getIn(['tally', 'thumbsDown']),
-    aCount: state.getIn(['tally', 'a']),
-    bCount: state.getIn(['tally', 'b']),
-    cCount: state.getIn(['tally', 'c']),
-    dCount: state.getIn(['tally', 'd']),
-    eCount: state.getIn(['tally', 'e']),
-    openResponseAnswers: state.getIn(['tally', 'answers']),
-    takingQuestions: state.get('takingQuestions'),
-    questions: state.get('questions'),
-    haveVoted: state.getIn(['tally', 'haveVoted']),
-    handRaised: state.get('handRaised'),
-    choice: state.get('choice'),
-    id: state.get('id'),
-    name: state.get('name'),
-    roomName: state.get('roomName'),
-    showgraph: state.get('showgraph'),
-    errMessage: state.get('errMessage'),
-    numUsers: state.get('numUsers'),
-    questionType: state.get('questionType'),
-    tally: state.get('tally'),
-    shareThumbsCheckResults: state.get('shareThumbsCheckResults')
- }
-}
+// function mapStateToProps(state) {
+//  return {
+//     userType: state.get('userType'),
+//     currentRoom : state.get('currentRoom'),
+//     voting: state.get('voting'),
+//     upCount: state.getIn(['tally', 'thumbsUp']),
+//     downCount: state.getIn(['tally', 'thumbsDown']),
+//     aCount: state.getIn(['tally', 'a']),
+//     bCount: state.getIn(['tally', 'b']),
+//     cCount: state.getIn(['tally', 'c']),
+//     dCount: state.getIn(['tally', 'd']),
+//     eCount: state.getIn(['tally', 'e']),
+//     openResponseAnswers: state.getIn(['tally', 'answers']),
+//     takingQuestions: state.get('takingQuestions'),
+//     questions: state.get('questions'),
+//     haveVoted: state.getIn(['tally', 'haveVoted']),
+//     handRaised: state.get('handRaised'),
+//     choice: state.get('choice'),
+//     id: state.get('id'),
+//     name: state.get('name'),
+//     roomName: state.get('roomName'),
+//     showgraph: state.get('showgraph'),
+//     errMessage: state.get('errMessage'),
+//     numUsers: state.get('numUsers'),
+//     questionType: state.get('questionType'),
+//     tally: state.get('tally'),
+//     shareThumbsCheckResults: state.get('shareThumbsCheckResults')
+//  }
+// }
 
 export const MainLandingContainer = connect(mapStateToProps, actionCreators)(Main);
