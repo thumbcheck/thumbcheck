68b1ff0fac27d5f9dd3f92721facdca3184aba57
Lex <lex.cooke@gmail.com>
2016-02-04 11:51:21 -0800

    add specific props to ResultsDisplay comp in EdMain

--- client/src/components/educator/EducatorMain.jsx
+++ client/src/components/educator/EducatorMain.jsx
@@ -37,7 +37,13 @@ export const Educator = React.createClass({
               <div><strong>{this.props.numUsers}</strong> device(s) currently in this room</div>
             </h4>
             {this.props.voting ?
-              <ResultsDisplay ref="resultsDisplay" {...this.props} /> :
+              <ResultsDisplay ref="resultsDisplay"  openResponseAnswers={this.props.openResponseAnswers}
+                                                    questionType={this.props.questionType}
+                                                    stopVote={this.props.stopVote}
+                                                    upCount={this.props.upCount}
+                                                    downCount={this.props.downCount}
+                                                    takingQuestions={this.props.takingQuestions}
+                                                    toggleTakingQuestions={this.props.toggleTakingQuestions} /> :
               <EducatorRequestCheckin {...this.props} />
             }
           </div>
