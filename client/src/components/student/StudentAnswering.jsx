3e6f5f9372f15082fa6464c9e3449aaa2f906d40
m-sandusky <msandusky08@gmail.com>
2016-02-05 12:18:39 -0800

    Adds content on student side for planned presentation

--- client/src/components/student/StudentAnswering.jsx
+++ client/src/components/student/StudentAnswering.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import checkHasVoted from '../../helpers/checkHasVoted';
 import renderCorrectStudentInput from '../../helpers/renderCorrectStudentInput';
+import StudentQuestionInformation from '../loggedin/StudentQuestionInformation';
 
 export default React.createClass({
   render: function() {
     console.log('question type in student', this.props.questionType);
     return (
       <div className="answering">
+        {this.props.preplannedPresentation ? <StudentQuestionInformation {...this.props} /> : null}
         {renderCorrectStudentInput(this.props)}
       </div>
     );
