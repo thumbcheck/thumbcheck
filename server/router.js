5b034385a2a00c86b461b7a332696e740a3c5a3a
m-sandusky <msandusky08@gmail.com>
2016-02-04 21:26:58 -0800

    Implements basic local auth

--- server/router.js
+++ server/router.js
@@ -5,6 +5,7 @@ import app from './server';
 import path from 'path';
 import randomWord from 'random-word';
 import generateRoomName from './helpers/generateRoomName';
+import passport from 'passport';
 
 import {checkRoom} from './controllers/redisStateController';
 
@@ -27,6 +28,35 @@ router.route('/room')
     res.send(roomName+'?type=host');
   });
 
+//*** THESE ARE PLACEHOLDER TEST ITEMS ***//
+// router.route('/logintest')
+//   .get((req,res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/testLogin.html"));
+//   });
+// router.route('/success')
+//   .get((req,res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/success.html"));
+//   });
+
+// router.route('/fail')
+//   .get((req,res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/fail.html"));
+//   });
+
+router.route('/login')
+  .get((req,res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/testLoginForm.html"));
+  })
+  .post((req,res,next) => {
+    return passport.authenticate('local-login', {successRedirect: '/success', failureRedirect: '/fail'})(req,res, next);
+  });
+
+router.route('/signup')
+  .get((req,res,next) => {
+    return passport.authenticate('local-success', {successRedirect: '/success', failureRedirect: '/fail'})(req,res, next);
+    // res.sendFile(path.join(__dirname, "../client/dist/testLoginSignupForm.html"));
+  });
+
 router.route('/:roomname')
   .get((req, res) => {
     res.sendFile(path.join(__dirname, "/../client/dist/index.html"));
@@ -38,5 +68,16 @@ router.route('/:roomname')
       res.send(boolean);
     });
   });
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 
   export default router;
