19e36f53cc88ec4bb8dca8ebdf5dd78ccc2a9487
m-sandusky <msandusky08@gmail.com>
2016-02-06 11:58:37 -0800

    Adds request helper function file Adds asynch actions to action_creators

---
+++ client/src/helpers/apiFunctions.js
@@ -0,0 +1,125 @@
+// GET presentation list for a user
+// GET questions for a given presentation
+// POST a new question for a given presentation 
+// POST a new presentation for a user
+// PUT a question in a given presentation 
+// PUT an existing presentation
+// DELETE a question in a given presentation
+// DELETE an existing presentation  
+
+// GET presentation list for an educator
+export function getUserPresentations(educatorID) {
+  $.ajax({
+      type: 'GET',
+      url: '/api/presentations/users/' + educatorID,
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// GET questions for a given presentation
+export function getPresentation(presentationID) {
+  $.ajax({
+      type: 'GET',
+      url: '/api/presentations/' + presentationID,
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// POST a new question for a given presentation (**Needs to include presentation id and educator id in data**)
+export function addPresentationQuestion(questionData) {
+  $.ajax({
+      type: 'POST',
+      url: '/api/questions',
+      data: questionData
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// POST a new presentation for a user (**Needs to include educator id in data**)
+export function addPresentation(presentationData) {
+  $.ajax({
+      type: 'POST',
+      url: '/api/presentations',
+      data: presentationData
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// PUT a question in a given presentation (**Needs to include presentation id and educator id in data**)
+export function editPresentationQuestion(questionData) {
+  $.ajax({
+      type: 'PUT',
+      url: /**NEED TO ADD**/'',
+      data: questionData
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// PUT an existing presentation (**Needs to include educator id in data**)
+export function editPresentation(presentationData) {
+  $.ajax({
+      type: 'PUT',
+      url: /**NEED TO ADD**/'',
+      data: presentationData
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// DELETE a question in a given presentation
+export function deletePresentationQuestion(questionID) {
+  $.ajax({
+      type: 'DELETE',
+      url: /**NEED TO ADD**/'',
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
+
+// DELETE an existing presentation  
+export function deletePresentation(presentationID) {
+  $.ajax({
+      type: 'DELETE',
+      url: /**NEED TO ADD**/'',
+      data: presentationData
+    })
+    .success(function(data) {
+      console.log(data);
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error: ', qXHR, textStatus, errorThrown);
+    });
+}
