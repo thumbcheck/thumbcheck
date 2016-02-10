3e5e6710d089c34e93404cfb25120ee75214e27d
m-sandusky <msandusky08@gmail.com>
2016-02-09 18:10:47 -0800

    Adds redis controller function to delete room in database

--- server/controllers/redisStateController.js
+++ server/controllers/redisStateController.js
@@ -69,3 +69,13 @@ export function checkRoom(room, callback) {
     client.quit();
   });
 }
+
+export function deleteRoom(room, callback) {
+  const client = redis.createClient();
+  client.del(room, (err, reply) => {
+    if (err) throw new Error(err);
+
+    console.log(reply);
+    client.quit();
+  });
+}
