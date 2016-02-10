45ec3c1edd7fece2aebb24a7bdec177618275127
Lex <lex.cooke@gmail.com>
2016-02-10 11:44:34 -0800

    default grey color AddPresName form buttons

--- client/src/components/loggedin/AddPresentationNameForm.jsx
+++ client/src/components/loggedin/AddPresentationNameForm.jsx
@@ -15,7 +15,7 @@ export default React.createClass({
       <div>
         <h3>Presentation Title:</h3>
         <input type="text" name="presentationTitle" placeholder='' onChange={this.handlePresentationTitleChange} />
-        <button type='button btn-info' className="btn" onClick={this.createNewPresentationTitle}>Submit</button>
+        <button className="btn" onClick={this.createNewPresentationTitle}>Submit</button>
         <button className="btn" onClick={ this.props.createOrEditPresentation }>Cancel</button>
       </div>
     )
