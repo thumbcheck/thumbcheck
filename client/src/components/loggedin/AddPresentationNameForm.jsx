591772da312c961a6043b1de817a290661192d4e
henry <hlyford11@gmail.com>
2016-02-11 15:16:19 -0800

    Create presentation page

--- client/src/components/loggedin/AddPresentationNameForm.jsx
+++ client/src/components/loggedin/AddPresentationNameForm.jsx
@@ -17,11 +17,12 @@ export default React.createClass({
   render: function() {
     console.log('add pres page', this.props);
     return (
-      <div>
-        <h3>Presentation Title:</h3>
-        <input type="text" name="presentationTitle" placeholder='' ref={(ref) => this.presentationInput = ref} onKeyDown ={this.handleEnter}  />
-        <button className="btn grey white-text" onClick={this.handleSubmit} >Submit</button>
-        <button className="btn light-grey white-text" onClick={ this.props.createOrEditPresentation }>Cancel</button>
+      <div className="create-presentation-container">
+        <div className="bigger-text push-bottom">Name Your Presentation</div>
+        <input className="push-right big-text big-input" type="text" name="presentationTitle" placeholder='' ref={(ref) => this.presentationInput = ref} onKeyDown ={this.handleEnter}  />
+        <button className="btn orange white-text push-right big-text align-up" onClick={this.handleSubmit} >Create</button>
+        <img className="next-icon align-up" src="/images/icons/cancel_icon1.png" onClick={ this.props.createOrEditPresentation }/><br /> 
+        {/*<button className="btn orange white-text" onClick={ this.props.createOrEditPresentation }>Go back</button> */}
       </div>
     )
   }
