83bd088dd5383c4d48b5e77276c789a0d64ae52f
Kris Albert Lee <krisalbert@gmail.com>
2016-02-10 16:27:38 -0800

    Bind enter key in AddPresentationForm

--- client/src/components/loggedin/AddPresentationNameForm.jsx
+++ client/src/components/loggedin/AddPresentationNameForm.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import {toJS} from 'immutable';
 
 export default React.createClass({
-  createNewPresentationTitle: function() {
-    let presentationTitle = this.presentationTitle;
-    this.props.addPresentation({title:presentationTitle, educator_id: this.props.educatorID});
+  handleSubmit: function (e) {
+    e.preventDefault();
+    const inputValue = this.presentationInput.value;
+    this.props.addPresentation({title:inputValue, educator_id: this.props.educatorID});
   },
-  handlePresentationTitleChange: function(e) {
-    this.presentationTitle = e.target.value;
+
+  handleEnter: function (e){
+    if (e.keyCode == 13){
+      this.handleSubmit(e);
+    }
   },
+
   render: function() {
     console.log('add pres page', this.props);
     return (
       <div>
         <h3>Presentation Title:</h3>
-        <input type="text" name="presentationTitle" placeholder='' onChange={this.handlePresentationTitleChange} />
-        <button className="btn grey white-text" onClick={this.createNewPresentationTitle}>Submit</button>
+        <input type="text" name="presentationTitle" placeholder='' ref={(ref) => this.presentationInput = ref} onKeyDown ={this.handleEnter}  />
+        <button className="btn grey white-text" onClick={this.handleSubmit} >Submit</button>
         <button className="btn light-grey white-text" onClick={ this.props.createOrEditPresentation }>Cancel</button>
       </div>
     )
